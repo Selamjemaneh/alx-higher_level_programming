ae3cf849f8ccc6ab9e123a3b836d722ebc679fb2
Selamjemaneh <selamjemaneh57@gmail.com>
2023-01-01 00:28:27 -0800

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sort with ascending order"""
+import MySQLdb
+from sys import argv
+db=MySQLdb.connect(user=argv[1],passwd=argv[2],db=argv[3],host="localhost",port=3306)
+c=db.cursor()
+c.execute("""SELECT * FROM states ORDER BY states.id""")
+a=c.fetchall()
+for i in a:
+    print(i)
